447a3ad344c986662d24645630048f4cf23b6fe3
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-01-25 21:55:07 +0500

    Make minor changes in adaptive design

--- js/app.js
+++ js/app.js
@@ -6,3 +6,15 @@ const sliderTwo = document.querySelector('.js-slider-two');
 sliderTop.appendChild(sliderOne.cloneNode(true));
 sliderBottom.appendChild(sliderTwo.cloneNode(true));
 
+
+const barBtn = document.querySelector('.js-bar-btn');
+
+barBtn.addEventListener('click', (evt) => {
+    window.scroll(10000, 0)
+});
+
+const closeBtn = document.querySelector('.js-close-btn');
+
+closeBtn.addEventListener('click', (evt) => {
+    window.scroll(0, 0);
+});
