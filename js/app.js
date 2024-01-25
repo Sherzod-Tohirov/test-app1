689a49afc299e384538d81cabb4bd27d8434c2a3
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-01-25 23:39:24 +0500

    Fix some issues

--- js/app.js
+++ js/app.js
@@ -2,19 +2,51 @@ const sliderTop = document.querySelector('.js-sw-top');
 const sliderOne = document.querySelector('.js-slider-one');
 const sliderBottom = document.querySelector('.js-sw-bottom');
 const sliderTwo = document.querySelector('.js-slider-two');
-
+let isOpen = false;
 sliderTop.appendChild(sliderOne.cloneNode(true));
 sliderBottom.appendChild(sliderTwo.cloneNode(true));
 
 
 const barBtn = document.querySelector('.js-bar-btn');
+const btnGroup = document.querySelector('.js-header-btn-group');
+const nav = document.querySelector('.js-nav');
 
 barBtn.addEventListener('click', (evt) => {
-    window.scroll(10000, 0)
+    btnGroup.style.display = 'flex';
+    nav.style.display = 'flex';
+    setTimeout(() => {
+        nav.style.right = 0;
+    }, 200);
+    isOpen = true;
 });
 
 const closeBtn = document.querySelector('.js-close-btn');
 
 closeBtn.addEventListener('click', (evt) => {
-    window.scroll(0, 0);
+    nav.style.right = "-100vw";
+    
+    setTimeout(() => {
+        btnGroup.style.display = 'none';
+        nav.style.display = 'none';
+    }, 200);
+    isOpen = false;
 });
+
+
+window.addEventListener('resize', (evt) => {
+    console.log(window.matchMedia('(min-width: 769px)').matches);
+   if(window.matchMedia('(min-width: 769px)').matches) {
+        btnGroup.style.display = 'flex';
+        nav.style.display = 'flex';
+        nav.style.right = 0;
+   }else {
+     if(isOpen) {
+        btnGroup.style.display = 'flex';
+        nav.style.display = 'flex';
+     }else {
+        btnGroup.style.display = 'none';
+        nav.style.display = 'none';
+     }
+   }
+});
+
